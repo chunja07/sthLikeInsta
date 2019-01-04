60439a25616a64bf6862115bf1712c7af875e6bc
402-24 <chunja07@gmail.com>
2019-01-04 17:24:21 +0900

    190102

---
+++ app/src/main/java/com/example/a402_24/day_03_register/HomeFragment.java
@@ -0,0 +1,34 @@
+package com.example.a402_24.day_03_register;
+
+
+import android.content.SharedPreferences;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import org.w3c.dom.Text;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        return v;
+    }
+
+}
